4fc34fc5e473e133938ed37221b59f348ae64bd6
mikolajmigacz <mig.mikolaj@gmail.com>
2025-11-04 21:13:49 +0100

    fix: CI

--- services/notification-service/jest.config.js
+++ services/notification-service/jest.config.js
@@ -1,12 +1,8 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.spec.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.spec.ts',
-    '!src/main.ts',
-  ],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts', '!src/main.ts'],
 };
